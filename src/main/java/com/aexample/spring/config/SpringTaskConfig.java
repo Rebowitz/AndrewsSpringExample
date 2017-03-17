470319bf378acab66e50d3b2be9e462616a8be54
(no author) <(no author)@43d33e46-ac77-44f2-9887-a1e30466e889>
2017-03-17 12:51:42 +0000

    Adding Task properties annotation
    git-svn-id: svn://192.168.0.76/usr/local/svn/repos/AndrewsSpringExample/trunk@56 43d33e46-ac77-44f2-9887-a1e30466e889

--- src/main/java/com/aexample/spring/config/SpringTaskConfig.java
+++ src/main/java/com/aexample/spring/config/SpringTaskConfig.java
@@ -1,12 +1,15 @@
 package com.aexample.spring.config;
 
 import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.PropertySource;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.scheduling.annotation.EnableScheduling;
+import org.springframework.stereotype.Component;
 
 @Configuration
 @EnableScheduling
 @ComponentScan({ "com.aexample.task" })
+@PropertySource({ "classpath:cron.properties" })
 public class SpringTaskConfig {
 
 }
